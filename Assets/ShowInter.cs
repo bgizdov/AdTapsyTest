8bee428e991eee4ca9e3cd2b76e61d309af13386
Mobinyaqoob <mobinyaqoob@live.com>
2015-04-10 13:29:12 +0500

    Adtapsy Test
    Its a testing version

---
+++ Assets/ShowInter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowInter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        AdTapsy.StartSessionAndroid("551b3c18e4b0eb64412af40a");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ShowIntertial()
+    {
+        AdTapsy.ShowInterstitial();  
+      
+    }
+}
