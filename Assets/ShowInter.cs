135208c9b687c5af006a664f53038efe168578c5
Borislav Gizdov <borislav@adtapsy.com>
2015-04-14 16:06:24 +0300

    Fixed AdTapsy integration

--- Assets/ShowInter.cs
+++ Assets/ShowInter.cs
@@ -5,7 +5,8 @@ public class ShowInter : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        AdTapsy.StartSessionAndroid("551b3c18e4b0eb64412af40a");
+		AdTapsy.StartSessionAndroid ("54982cf7e4b052cd2a20a7b8");
+		AdTapsy.StartSessionIOS ("539777bae4b02eacca4bcb67");
 	}
 	
 	// Update is called once per frame
